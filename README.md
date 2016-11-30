9124bd9288dda9e72545f1c030592c641fc837d6
ResonantWave <nixijav@nixijav.com>
2016-11-30 17:49:48 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# schematic_loader
+# Minecraft Pi Schematic Loader
 Loads a schematic into a Minecraft Pi world
+
+### Installation
+
+This program requires Tkinter. You can install it in Debian-based systems with `sudo apt-get install python-tk`. For all other dependencies, please check [requirements.txt](requirements.txt). They are installable via PIP.
+
+
+### Usage
+
+You can run this program with `python schematic.py`. The GUI will only appear when Minecraft is open and ready.
+
+![GUI](gui.png)
+### Contributors
+
+Development:
+ *  [@ResonantWave](https://github.com/ResonantWave)
+
+
+### Contributing
+
+* The code is licensed under the [GPL V3](LICENSE)
+* Feel free to contribute to the code
+
+
+### Other
+
+ * The original Minecraft Pi API, made by Mojang AB, is included for convenience reasons. 
+ * This tool was created for the Tinkercad and Minecraft workshop, made by [VermisLAB](http://www.vermislab.com/). 
+ * VermisLAB, and the VermisLAB logo are property of [Vermis Project](http://www.vermisproject.com/), and neither can be used without autorization.
---
+++ schematic.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Schematic.py: Loads an schematic in a Minecraft Pi world"""
+
+import Tkinter as tk
+from Tkinter      import *
+from ttk          import *
+from tkFileDialog import askopenfilename
+from socket       import error as socket_error
+from subprocess   import Popen
+import mcpi.minecraft as minecraft
+import nbt, time, sys, errno, os
+import Image, ImageTk, ttk
+
+class App:
+   def __init__(self, master):
+      """Handles all GUI related things"""
+      frame = Frame(master)
+      frame.pack()
+
+      master.wm_title('Cargador de modelos')
+
+      img = ImageTk.PhotoImage(Image.open(os.path.dirname(os.path.realpath(__file__)) + '/vermislablogo.png'))
+      logo = ttk.Label(frame, image = img)
+      logo.image = img
+      logo.grid(row = 0, column = 0, columnspan = 3)
+
+      separator = ttk.Separator(frame, orient = HORIZONTAL)
+      separator.grid(row = 1, column = 0, columnspan = 3, sticky = 'we')
+
+      self.button = ttk.Button(frame, text = 'Cargar modelo', command = self.openfile)
+      self.button.grid(row = 2, column = 2, padx = 5, pady = 5)
+
+      self.button = ttk.Button(frame, text = 'Aplanar mapa', command = self.flatland)
+      self.button.grid(row = 2, column = 1, padx = 5, pady = 5)
+
+      self.button = tk.Button(frame, text = 'Borrar modelo', command = self.delete, fg = 'red')
+      self.button.grid(row = 2, column = 0, padx = 5, pady = 5)
+
+      self.button = ttk.Button(frame, text = 'Teletransportarse a 0,0,0', command = self.teletozero)
+      self.button.grid(row = 3, column = 0, padx = 5, pady = 5, columnspan = 3)
+
+
+
+   def openfile(self):
+      """Displays the schematic chooser dialog and loads it into the game"""
+      global file
+      file = askopenfilename()
+      loadSchematic(file)
+
+   def delete(self):
+      """Deletes an already loaded schematic"""
+      global file, playerX, playerY, playerZ
+      loadSchematic(file, 1, playerX, playerY, playerZ)
+
+   def flatland(self):
+      """Flatens the world to simplify construction, or just for cleanup"""
+      mc.setBlocks(-128, -10, -128, 128, 64, 128, 0)
+      mc.setBlocks(-128, -10, -128, 128, -64, 128, 24)
+      mc.postToChat('Mapa aplanado!')
+
+   def teletozero(self):
+      """Teleports player to coordinate origin"""
+      mc.player.setPos(0, 0, 0)
+
+def loadSchematic(schematicToImport, deleteSchematic = 0, delX = 0, delY = 0, delZ = 0):
+   """
+   Loads schematic into the game.
+
+   Parameters:
+   ----------
+   schematicToImport : string
+      A string that represents the absolute or relative filepath of the schematic
+   deleteSchematic : int
+      Set to 1 to delete instead of loading. Defaults to 0
+   delX : int
+      Only used if deleteSchematic is set. Defines the x coordinate of the schematic to be deleted. Defaults to 0
+   delY : int
+      Only used if deleteSchematic is set. Defines the y coordinate of the schematic to be deleted. Defaults to 0
+   delZ : int
+      Only used if deleteSchematic is set. Defines the z coordinate of the schematic to be deleted. Defaults to 0
+   """
+   delete = True if (deleteSchematic == 1) else 0
+   infile = nbt.nbt.NBTFile(schematicToImport, 'rb')
+
+   if(infile['Width'] == "Classic"):
+      raise ImportError('Este formato de schematic no está soportado')
+
+   width = infile['Width'].value
+   length = infile['Length'].value
+   height = infile['Height'].value
+
+   global playerX, playerY, playerZ
+
+   if(delete == False):
+      playerX = playerY = playerZ = 0
+      playerX, playerY, playerZ = mc.player.getPos()
+      mc.postToChat('Cargando {}...'.format(schematicToImport))
+      mc.setBlocks(playerX,
+		   playerY,
+		   playerZ,
+		   playerX + width,
+		   playerY + height,
+		   playerZ + length,
+		   0)
+
+      index = 0
+
+      for x in range(width):
+         for y in range(height):
+            for z in range(length):
+               arrayPos = (y * length + z) * width + x
+               id = infile['Blocks'][arrayPos]
+               data = infile['Data'][arrayPos]
+               mc.setBlock(playerX + x,
+			   playerY + y,
+			   playerZ + z,
+			   0 if delete else id,
+			   data)
+               index += 1
+               if(id != 0):
+                  time.sleep(0.0001)
+               try:
+                  if(((float(index) / len(infile['Blocks'])) * 100) % 10 == 0):
+                     mc.postToChat('{}% cargado...'.format(str(index * 100 / len(infile['Blocks']))))
+               except ZeroDivisionError:
+                  continue
+
+      mc.postToChat('Modelo cargado!')
+   else:
+      mc.postToChat('Borrando {}...'.format(schematicToImport))
+      mc.setBlocks(delX,
+		   delY,
+		   delZ,
+		   delX + width,
+		   delY + height,
+		   delZ + length,
+		   0)
+      mc.postToChat('Modelo borrado!')
+
+#minecraftProcess = Popen([]) TODO: replace with minecraft pi absolute path
+
+print('Esperando a que Minecraft se inicie...')
+while 1:
+   try:
+      mc = minecraft.Minecraft.create()
+      try:
+         mc
+         break
+      except NameError:
+         continue
+   except socket_error as serr:
+      continue
+
+root = Tk()
+root.style = Style()
+root.style.theme_use('clam')
+
+app = App(root)
+root.mainloop()
